02c83f5b4b53ada065bcff85366bfdf6b9a9b412
dongjoo0-0 <kdj486347@gmail.com>
2023-08-15 21:45:03 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/gugucon/datageneration/DataGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.gugucon.datageneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataGenerationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataGenerationApplication.class, args);
+    }
+
+}
