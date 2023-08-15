f939860fe2ff317decec9999d965a7be5f5e4765
dongjoo0-0 <kdj486347@gmail.com>
2023-08-15 21:57:18 +0900

    refactor: 저장로직 효율화

--- src/main/java/com/gugucon/datageneration/application/ProductService.java
+++ src/main/java/com/gugucon/datageneration/application/ProductService.java
@@ -4,23 +4,20 @@ import com.gugucon.datageneration.infrastructure.CSVReader;
 import com.gugucon.datageneration.repository.ProductRepository;
 import com.gugucon.datageneration.domain.Product;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
     private final CSVReader<Product> csvReader;
 
-    public ProductService(final ProductRepository productRepository, final CSVReader<Product> csvReader) {
-        this.productRepository = productRepository;
-        this.csvReader = csvReader;
-    }
-
     public int createData(String path) {
         List<Product> products = csvReader.readCSV(path);
         System.out.println("파싱 완료");
-        products.stream().forEach(productRepository::save);
+        productRepository.saveAll(products);
         return products.size();
     }
 }
